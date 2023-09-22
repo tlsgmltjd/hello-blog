02d1e7c58c7e44adc2b004b0d7e16221fb8a3660
신희성 <vvwv3092@gmail.com>
2023-09-22 12:25:28 +0900

    Add Test(Controller, Service, Repository, Entity)

---
+++ src/main/java/com/blog/helloblog/Member.java
@@ -0,0 +1,19 @@
+package com.blog.helloblog;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
