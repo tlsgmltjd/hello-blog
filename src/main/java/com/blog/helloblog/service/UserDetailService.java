e20f681562515640fbc0bb4090324321d9c3828c
신희성 <vvwv3092@gmail.com>
2023-09-30 22:39:30 +0900

    Add Security - UserService

---
+++ src/main/java/com/blog/helloblog/service/UserDetailService.java
@@ -0,0 +1,22 @@
+package com.blog.helloblog.service;
+
+import com.blog.helloblog.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    // 사용자 이름(email)으로 사용자의 정보를 가져오는 메서드
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException(email));
+    }
+}
