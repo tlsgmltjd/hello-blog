b3f6fbf8acc37cbaa4734f50cd2dadfa99adce3b
신희성 <vvwv3092@gmail.com>
2023-10-04 18:15:40 +0900

    Add RefreshToken

---
+++ src/main/java/com/blog/helloblog/domain/RefreshToken.java
@@ -0,0 +1,18 @@
+package com.blog.helloblog.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    // ...
+}
