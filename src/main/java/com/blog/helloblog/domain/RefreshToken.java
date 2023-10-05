2b5807096a392bcb38c77bcf0477c86efa0d5f05
신희성 <vvwv3092@gmail.com>
2023-10-05 20:32:52 +0900

    Add RefreshTokem Entity, Repository

--- src/main/java/com/blog/helloblog/domain/RefreshToken.java
+++ src/main/java/com/blog/helloblog/domain/RefreshToken.java
@@ -14,5 +14,19 @@ public class RefreshToken {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    // ...
+    @Column(name = "user_id", nullable = false, unique = true)
+    private Long userId;
+
+    @Column(name = "refresh_token", nullable = false)
+    private String refreshToken;
+
+    public RefreshToken(Long userId, String refreshToken) {
+        this.userId = userId;
+        this.refreshToken = refreshToken;
+    }
+
+    public RefreshToken update(String newRefreshToken) {
+        this.refreshToken = newRefreshToken;
+        return this;
+    }
 }
---
+++ src/main/java/com/blog/helloblog/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.blog.helloblog.repository;
+
+import com.blog.helloblog.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
